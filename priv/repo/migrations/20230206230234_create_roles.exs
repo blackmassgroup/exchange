1cd72b3855cee7913b0e7f7cc60691f6f288a479
John Herbener <herbener.dev@gmail.com>
2023-02-06 21:27:00 -0500

    Fix migration

--- priv/repo/migrations/20230206230234_create_roles.exs
+++ priv/repo/migrations/20230206230234_create_roles.exs
@@ -2,18 +2,12 @@ defmodule VxUnderground.Repo.Migrations.CreateRoles do
   use Ecto.Migration
   alias VxUnderground.Accounts
 
-  def up do
+  def change do
     create table(:roles) do
       add :name, :string
       add :permissions, :map
 
       timestamps()
     end
-
-    Enum.map(Accounts.DefaultRoles.all(), &Accounts.create_role(&1))
-  end
-
-  def down do
-    drop table(:roles)
   end
 end
