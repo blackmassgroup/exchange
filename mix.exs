5b6dbd2afc9cac6862190110405e6ee5dcad673a
John Herbener <herbener.dev@gmail.com>
2023-03-03 18:40:24 -0500

    Update deps

--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule VxUnderground.MixProject do
   defp deps do
     [
       {:bcrypt_elixir, "~> 3.0"},
-      {:phoenix, "~> 1.7.0-rc.2", override: true},
+      {:phoenix, "~> 1.7.1", override: true},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
       {:postgrex, ">= 0.0.0"},
