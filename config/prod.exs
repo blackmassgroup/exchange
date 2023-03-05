f80f261a9cd2dd67be18839dfd3cf633a7cc6b98
John Herbener <herbener.dev@gmail.com>
2023-03-05 18:51:33 -0500

    Distributed Swoosh adaptors

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ config :vx_underground, VxUndergroundWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Configures Swoosh API Client
-config :swoosh, api_client: Swoosh.ApiClient.Finch, finch_name: VxUnderground.Finch
+config :swoosh, api_client: Swoosh.ApiClient.Finch, finch_name: VxUnderground.Finch, local: false
 
 # Do not print debug messages in production
 config :logger, level: :info
