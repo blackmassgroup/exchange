4ce7838a364bd6762d11bc1072d3377a46954d94
John Herbener <herbener.dev@gmail.com>
2023-03-11 13:42:21 -0500

    Update setup script

--- mix.exs
+++ mix.exs
@@ -75,7 +75,7 @@ defmodule VxUnderground.MixProject do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      setup: ["deps.get", "ecto.setup", "assets.setup", "assets.build"],
+      setup: ["deps.get", "ecto.setup", "assets.setup", "assets.build", "run priv/repo/seeds.exs"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
