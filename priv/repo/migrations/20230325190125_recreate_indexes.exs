0d10dbd8c17b3336ed15b38a7e38316786817722
John Herbener <herbener.dev@gmail.com>
2023-03-25 17:52:45 -0400

    Comment out migration to be run manually

--- priv/repo/migrations/20230325190125_recreate_indexes.exs
+++ priv/repo/migrations/20230325190125_recreate_indexes.exs
@@ -2,14 +2,14 @@ defmodule VxUnderground.Repo.Local.Migrations.RecreateIndexes do
   use Ecto.Migration
 
   def change do
-    drop_if_exists unique_index(:samples, :sha1)
-    drop_if_exists unique_index(:samples, :sha256)
-    drop_if_exists unique_index(:samples, :sha512)
-    drop_if_exists unique_index(:samples, :md5)
+    # drop_if_exists unique_index(:samples, :sha1)
+    # drop_if_exists unique_index(:samples, :sha256)
+    # drop_if_exists unique_index(:samples, :sha512)
+    # drop_if_exists unique_index(:samples, :md5)
 
-    create unique_index(:samples, :sha1)
-    create unique_index(:samples, :sha256)
-    create unique_index(:samples, :sha512)
-    create unique_index(:samples, :md5)
+    # create unique_index(:samples, :sha1)
+    # create unique_index(:samples, :sha256)
+    # create unique_index(:samples, :sha512)
+    # create unique_index(:samples, :md5)
   end
 end
