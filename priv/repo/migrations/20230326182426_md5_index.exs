b5b78f3c6b2440f069a8fefe81675711df11c928
John Herbener <herbener.dev@gmail.com>
2023-03-26 14:26:23 -0400

    MD5 Index test
    See if this gets passed the CI

---
+++ priv/repo/migrations/20230326182426_md5_index.exs
@@ -0,0 +1,8 @@
+defmodule VxUnderground.Repo.Local.Migrations.Md5Index do
+  use Ecto.Migration
+
+  def change do
+    drop_if_exists unique_index(:samples, :md5)
+    create unique_index(:samples, :md5)
+  end
+end
