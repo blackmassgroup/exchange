2f65513dafc3be4376c88009188ff5275102a932
John Herbener <herbener.dev@gmail.com>
2023-04-05 17:37:10 -0400

    fix triage

--- lib/vx_underground_web/live/sample_live/show.ex
+++ lib/vx_underground_web/live/sample_live/show.ex
@@ -7,49 +7,48 @@ defmodule VxUndergroundWeb.SampleLive.Show do
   import VxUndergroundWeb.SampleLive.Index, only: [generate_url_for_file: 1]
 
   @impl true
-  def mount(_params, _session, socket) do
-    {:ok, socket}
-  end
+  def mount(%{"id" => sample_id} = _params, _session, socket) do
+    if connected?(socket) do
+      sample = Samples.get_sample(sample_id)
 
-  @impl true
-  def handle_params(%{"id" => id}, _, socket) do
-    sample = Samples.get_sample(id)
+      case sample do
+        nil ->
+          {:noreply,
+           put_flash(socket, :error, "Sample does not exist") |> push_navigate(to: ~p(/samples))}
 
-    case sample do
-      nil ->
-        {:noreply,
-         put_flash(socket, :error, "Sample does not exist") |> push_navigate(to: ~p(/samples))}
+        _ ->
+          virus_total =
+            case VirusTotal.get_sample(sample.sha256) do
+              {:ok, virus_total} ->
+                virus_total
 
-      _ ->
-        virus_total =
-          case VirusTotal.get_sample(sample.sha256) do
-            {:ok, virus_total} ->
-              virus_total
+              {:error, _} ->
+                :does_not_exist
+            end
 
-            {:error, _} ->
-              :does_not_exist
-          end
+          triage =
+            case TriageSearch.search(sample.sha256) do
+              {:ok, %{"data" => data}} ->
+                data
 
-        triage =
-          case TriageSearch.search(sample.sha256) do
-            {:ok, %{"data" => data}} ->
-              data
+              {:error, _} ->
+                %{sample: sample}
+                |> VxUnderground.ObanJobs.TriageUpload.new()
+                |> Oban.insert()
 
-            {:error, _} ->
-              %{sample: sample}
-              |> VxUnderground.ObanJobs.TriageUpload.new()
-              |> Oban.insert()
+                :still_processing
+            end
 
-              :still_processing
-          end
-
-        {:noreply,
-         socket
-         |> assign(:page_title, page_title(socket.assigns.live_action))
-         |> assign(:sample, sample)
-         |> assign(:tags, Tags.list_tags() |> Enum.map(&[value: &1.id, key: &1.name]))
-         |> assign(:virus_total, virus_total)
-         |> assign(:triage, triage)}
+          {:ok,
+           socket
+           |> assign(:page_title, page_title(socket.assigns.live_action))
+           |> assign(:sample, sample)
+           |> assign(:tags, Tags.list_tags() |> Enum.map(&[value: &1.id, key: &1.name]))
+           |> assign(:virus_total, virus_total)
+           |> assign(:triage, triage)}
+      end
+    else
+      {:ok, socket}
     end
   end
 
