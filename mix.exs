386162c34d5816b0cacef9a776d04e6b8e3ca981
John Herbener <herbener.dev@gmail.com>
2023-05-08 18:46:26 -0400

    adjust ecto setup

--- mix.exs
+++ mix.exs
@@ -76,7 +76,12 @@ defmodule VxUnderground.MixProject do
   defp aliases do
     [
       setup: ["deps.get", "ecto.setup", "assets.setup", "assets.build"],
-      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      "ecto.setup": [
+        "ecto.create",
+        "ecto.load --skip-if-loaded --quiet",
+        "ecto.migrate",
+        "run priv/repo/seeds.exs"
+      ],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
       "test.ci": [
