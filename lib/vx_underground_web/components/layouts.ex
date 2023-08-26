413a980aebf39a980dd9cb30b19ee459414ab602
John Herbener <herbener.dev@gmail.com>
2023-08-26 14:10:12 -0400

    App layout with mobile menu toggle

--- lib/vx_underground_web/components/layouts.ex
+++ lib/vx_underground_web/components/layouts.ex
@@ -2,4 +2,32 @@ defmodule VxUndergroundWeb.Layouts do
   use VxUndergroundWeb, :html
 
   embed_templates "layouts/*"
+
+  def toggle_mobile_menu() do
+    JS.toggle(
+      to: "#mobile-menu-canvas-backdrop",
+      in: {"transition-opacity ease-linear duration-300", "opacity-0", "opacity-100"},
+      out: {"transition-opacity ease-linear duration-300", "opacity-100", "opacity-0"}
+    )
+    |> JS.toggle(
+      to: "#mobile-menu-fixed-insert",
+      in: {"transition-opacity ease-linear duration-300", "opacity-0", "opacity-100"},
+      out: {"transition-opacity ease-linear duration-300", "opacity-100", "opacity-0"}
+    )
+    |> JS.toggle(
+      to: "#mobile-menu-canvas-menu-translate",
+      in: {"transition ease-in-out duration-300 transform", "-translate-x-full", "translate-x-0"},
+      out: {"transition ease-in-out duration-300 transform", "translate-x-0", "-translate-x-full"}
+    )
+    |> JS.toggle(
+      to: "#mobile-menu-close",
+      in: {"ease-in-out duration-300", "opacity-0", "opacity-100"},
+      out: {"ease-in-out duration-300", "opacity-100", "opacity-0"}
+    )
+    |> JS.toggle(
+      to: "#mobile-menu-canvas-menu",
+      in: {"transition-opacity ease-linear duration-300", "opacity-0", "opacity-100"},
+      out: {"transition-opacity ease-linear duration-300", "opacity-100", "opacity-0"}
+    )
+  end
 end
