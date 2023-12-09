badceaa737aef39382c4bb64118ab63e7df581b6
John Herbener <herbener.dev@gmail.com>
2023-12-09 10:54:53 -0500

    Revert elixir versions for fly connectivity bug

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule VExchange.MixProject do
     [
       app: :v_exchange,
       version: "2.3.0",
-      elixir: "~> 1.15",
+      elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
