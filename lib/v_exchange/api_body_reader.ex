0a2f5ccc232ef423e06802e012fa3ca48b381298
guessthepw <john@herbener.cloud>
2024-08-24 13:48:47 -0400

    Fixup api body reader

--- lib/v_exchange/api_body_reader.ex
+++ lib/v_exchange/api_body_reader.ex
@@ -1,25 +1,36 @@
 defmodule VExchange.ApiBodyReader do
-  def read_body(%Plug.Conn{request_path: path} = conn, opts)
-      when path in ["/api/upload", "/api/samples/new"] do
-    new_opts = [read_length: 52_000_000, length: 52_000_000, read_timeout: 20_000]
-    opts = Keyword.merge(opts, new_opts)
+  @upload_paths ["/api/upload", "/api/samples/new"]
 
-    {:ok, body, conn} =
-      Plug.Conn.read_body(conn, opts)
-      |> case do
-        {:ok, body, conn} -> {:ok, body, conn}
-        {:more, data} -> {:ok, data, conn}
-        {:more, data, conn} -> {:ok, data, conn}
-      end
+  def read_body(%Plug.Conn{request_path: path} = conn, opts) when path in @upload_paths do
+    size = VExchange.Samples.size_limit()
+    addl_opts = [read_length: size, length: size, read_timeout: 20_000]
+    opts = Keyword.merge(opts, addl_opts)
 
-    params = %{"file" => body}
-    conn = Map.put(conn, :params, params)
-
-    {:ok, "", conn}
+    Plug.Conn.read_body(conn, opts)
+    |> handle_read(conn, addl_opts)
   end
 
   def read_body(conn, opts) do
-    {:ok, body, conn} = Plug.Conn.read_body(conn, opts)
+    Plug.Conn.read_body(conn, opts)
+  end
+
+  defp handle_read({:ok, body, conn}, _conn, _opts) do
+    conn = update_params(conn, body)
     {:ok, body, conn}
+  end
+
+  defp handle_read({:more, partial_body, conn}, _original_conn, opts) do
+    {:ok, body, conn} = read_body(conn, opts)
+    conn = update_params(conn, partial_body <> body)
+    {:ok, partial_body <> body, conn}
+  end
+
+  defp handle_read({:error, _reason}, conn, _opts) do
+    {:ok, "", conn}
+  end
+
+  defp update_params(conn, body) do
+    params = %{"file" => body}
+    Map.put(conn, :params, params)
   end
 end
