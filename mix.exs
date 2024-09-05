3ed6f94f85ba7fce65c6ee3f48c44fcaa7f0236d
guessthepw <john@herbener.cloud>
2024-09-04 20:31:19 -0400

    update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule VExchange.MixProject do
   def project do
     [
       app: :v_exchange,
-      version: "2024.9.2",
+      version: "2024.9.4",
       elixir: "~> 1.17",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
