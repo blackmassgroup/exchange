b16bbc241f1d8d9a99946bdba2f9e2d5ed996aaa
guessthepw <john@herbener.cloud>
2024-09-08 12:42:33 -0400

    Clean samples PT 2

--- lib/v_exchange/oban_jobs/clean_samples.ex
+++ lib/v_exchange/oban_jobs/clean_samples.ex
@@ -3,13 +3,49 @@ defmodule VExchange.ObanJobs.CleanSamples do
   use Oban.Worker, queue: :clean_samples, max_attempts: @max_attempts
 
   def perform(%Oban.Job{args: %{"sha256" => sha256}}) do
-    # For all samples in database
+    # Step 1: Check if the file is malformed
+    # with {:ok, %{body: body}} <- VExchange.Services.S3.get_file_binary(sha256),
+    #      true <-
+    #        String.contains?(body, ~S(name=\"file\";)) or
+    #          String.contains?(body, "Content-Disposition: form-data;") or
+    #          String.contains?(body, ~S("file\":)) do
+    #   VExchange.MalformedSamples.create_malformed_sample(%{sha256: sha256})
+    # end
+
+    # Step 2: Clean the file
     with {:ok, %{body: body}} <- VExchange.Services.S3.get_file_binary(sha256),
-         true <-
-           String.contains?(body, ~S(name=\"file\";)) or
-             String.contains?(body, "Content-Disposition: form-data;") or
-             String.contains?(body, ~S("file\":)) do
-      VExchange.MalformedSamples.create_malformed_sample(%{sha256: sha256})
+         cleaned_binary <- clean_file(body) do
+      VExchange.Samples.create_from_binary(%{"file" => cleaned_binary}, 516)
+      |> case do
+        {:ok, sample} ->
+          MalformedSamples.get_malformed_sample_by_sha(sample.sha256)
+          |> MalformedSamples.update_malformed_sample(%{sample_id: sample.id})
+
+        error ->
+          error
+      end
+    end
+  end
+
+  def clean_file(binary) when is_binary(binary) do
+    binary
+    |> remove_starting_boundary()
+    |> remove_ending_boundary()
+  end
+
+  defp remove_starting_boundary(binary) do
+    case :binary.split(binary, "\r\n\r\n") do
+      [_header, content | _] -> content
+      # Return original if pattern doesn't match
+      _ -> binary
+    end
+  end
+
+  defp remove_ending_boundary(binary) do
+    case :binary.split(binary, "\r\n--") do
+      [content, _footer] -> content
+      # Return original if pattern doesn't match
+      _ -> binary
     end
   end
 end
